b24e5cc1ff669d785242355269db995789cd3e37
carlosjepard <f6carlos.afonso@gmail.com>
2024-01-30 10:36:33 +0000

    added technical metadata

---
+++ roda-core/roda-core/src/main/java/org/roda/core/common/TechnicalMetadataElement.java
@@ -0,0 +1,10 @@
+package org.roda.core.common;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * @author Carlos Afonso <cafonso@keep.pt>
+ */
+@XmlRootElement(name = "metadata")
+public abstract class TechnicalMetadataElement {
+}
